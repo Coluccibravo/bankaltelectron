dfde9defe6ac10cc834a8cbf41231ce810b22668
Nucleo de Informatica <ni@cotil.unicamp.br>
2025-02-28 09:38:52 -0300

    Criado mais uma tela Sisdev1 e Atualização nas outras telas já existentes

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
 document.querySelector('#aPageIntroduction')?.addEventListener('click', ()=>{
     window.location.href = './src/login.html';
 }) || console.log("Errou mermao")
+
+
+document.querySelector('#btninfo')?.addEventListener('click', ()=>{
+    window.location.href = './src/sistemDev1.html';
+}) || console.log("Errou mermao")
